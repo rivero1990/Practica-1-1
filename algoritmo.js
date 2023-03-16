c86ba1ebf7d92eb81b0543365e9da95966c50918
Anibal Rivero <riveroleonel@gmail.com>
2023-03-16 19:47:30 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+document. querySelector("body"). innerHTML = `
+<h1>titulo practica 1-1</h1>
+    <h2>titulo h2</h2>
+    <h3>titulo h3</h3>
+
+    <img src="https://th.bing.com/th/id/OIP.d8Eksrf-Ettsf4EXUcDf1wHaEk?w=258&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="foto de batman">
+
+    <img src="imagenes/dc comics.jpg" alt="foto de batman 2">
+`;
